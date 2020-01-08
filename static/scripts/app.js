39a7b7debe166559b746198827fa2b23246b176d
Thomas Wade <gigatech55@gmail.com>
2020-01-09 01:01:12 +1030

    Refresh Discord auth state when closing popup

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -18,11 +18,23 @@ var form = new Vue({
     },
     methods: {
         openDiscordPopout: function() {
-            window.open(
+            // Open the popup
+            var w = window.open(
                 'https://discordapp.com/api/oauth2/authorize?client_id=631115320823644180&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fdiscord%2Fcallback&response_type=code&scope=identify',
                 'Authorise with Discord',
                 'width=375,height=485,top=' + ((window.innerHeight / 2) - 200) + ',left=' + ((window.innerWidth / 2) - 187)
             );
+
+            // Handle the popup closing to refresh our authorised state
+            // Hacky, but it works for all origins, not just the site we're hosting
+            const vueInstance = this; // Necessary since 'this' becomes the DOM
+            var timer = setInterval(function() {
+                if (w.closed) {
+                    // Stop the timer and check our auth state
+                    clearInterval(timer);
+                    vueInstance.checkIfAuthorised();
+                }
+            }, 250);
         },
         checkIfAuthorised: function() {
             // Get the authorised status from the API and parse it as JSON
